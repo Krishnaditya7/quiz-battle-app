6c3c8370661871378036eb7c6903f29923b14b24
Krishna <krishnadityarai778@gmail.com>
2026-03-24 00:17:47 +0530

    done

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -13,8 +13,8 @@ const generateToken = (userId) => {
 const sendTokenCookie = (res, token) => {
     res.cookie('token', token, {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'false',
-        sameSite: 'lax',
+        secure: process.env.NODE_ENV === 'production',
+        sameSite: 'production' ? 'none' : 'lax',
         maxAge:  7* 24 * 60 * 60 * 1000,
     });
 
